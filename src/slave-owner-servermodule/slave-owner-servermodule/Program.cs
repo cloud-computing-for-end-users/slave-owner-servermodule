52675f5af1ad1a3f10e9090fadab47213e944318
gaticks <kenneth.norholm@gmail.com>
2019-09-11 10:18:46 +0200

    CC-45 inisial Slave owner servermodule structure

---
+++ src/slave-owner-servermodule/slave-owner-servermodule/Program.cs
@@ -0,0 +1,63 @@
+﻿using custom_message_based_implementation.consts;
+using custom_message_based_implementation.encoding;
+using custom_message_based_implementation.model;
+using message_based_communication.model;
+using System;
+using System.Collections.Generic;
+
+namespace slave_owner_servermodule
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slave owner servermodule is starting...");
+
+            checked
+            {
+                try
+                {
+                    var portToListenForRegistration = new Port() { ThePort = 5533 };
+
+                    var server_module_connection_informtion = new ConnectionInformation()
+                    {
+                        IP = new IP() { TheIP = "127.0.0.1" },
+                        Port = new Port() { ThePort = 5522 }
+                    };
+
+                    var slave_owner_module_conn_info = new ConnectionInformation()
+                    {
+                        IP = new IP() { TheIP = "127.0.0.1" },
+                        Port = new Port() { ThePort = 5532 }
+                    };
+
+                    var slaveOwner = new SlaveOwnerServermodule(portToListenForRegistration, new ModuleType() { TypeID = ModuleTypeConst.MODULE_TYPE_SLAVE_OWNER }, new CustomEncoder());
+                    slaveOwner.Setup(server_module_connection_informtion, new Port() { ThePort = 5523 }, slave_owner_module_conn_info, new CustomEncoder());
+
+                    Console.WriteLine("The Slave Owner Servermodule have started successfully");
+
+                    //test
+                    ////var list = slaveOwner.GetListOfRunnableApplications();
+                    ////var encoded = CustomEncoder.EncodeResponse(new Response()
+                    ////{
+                    ////    CallID = null,
+                    ////    Payload = new Payload() { ThePayload = new List<ApplicationInfo>()},
+                    ////    SenderModuleID = null,
+                    ////    TargetModuleID = null
+                    ////});
+
+                    ////var decoded = new CustomEncoder().DecodeIntoSendable(encoded); ;
+                    ////var response = decoded as Response;
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The slave owner Servermoudle encountered an exception while in the setup process: " + ex.Message);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ src/slave-owner-servermodule/slave-owner-servermodule/SlaveOwnerServermodule.cs
@@ -0,0 +1,72 @@
+﻿using custom_message_based_implementation.custom_request.slave_owner_servermodule;
+using custom_message_based_implementation.interfaces;
+using custom_message_based_implementation.model;
+using message_based_communication.encoding;
+using message_based_communication.model;
+using message_based_communication.module;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace slave_owner_servermodule
+{
+    public class SlaveOwnerServermodule : BaseRouterModule, ISlaveOwnerServermodule
+    {
+        public SlaveOwnerServermodule(Port portForRegistrationToRouter, ModuleType moduleType, message_based_communication.encoding.Encoding customEncoding) : base(portForRegistrationToRouter, moduleType, customEncoding)
+        {
+        }
+
+        public override string CALL_ID_PREFIX => "SLAVE_OWNER_SM_CALL_ID_";
+
+        protected override string MODULE_ID_PREFIXES => "SLAVE_OWNER_SM_MODULE_ID_";
+
+
+        public override void HandleRequest(BaseRequest message)
+        {
+            object responsePayload = null;
+
+            if(message is RequestGetSlave _reqGetSlave)
+            {
+                responsePayload = GetSlave(_reqGetSlave.Arg2AppInfo, _reqGetSlave.Arg1PrimaryKey);
+            }
+            else if(message is RequestGetListOfRunningApplications _GetListOFApps)
+            {
+                Console.WriteLine("Recived request for GetListOfRunningApplications");
+                responsePayload = GetListOfRunnableApplications();
+            }
+            else
+            {
+                throw new Exception("Reviced message that I don't know how to handle");
+            }
+
+            var response = GenerateResponseBasedOnRequestAndPayload(message, responsePayload);
+            SendResponse(response);
+
+        }
+
+
+        public List<ApplicationInfo> GetListOfRunnableApplications()
+        {
+            //todo FIX obviously
+            var list = new List<ApplicationInfo>();
+            list.Add(new ApplicationInfo() {ApplicationName ="kenneths app 1", ApplicationVersion = "Version awsome", RunningOnOperatingSystem="KOS1" });
+            list.Add(new ApplicationInfo() {ApplicationName ="kenneths app 2", ApplicationVersion = "Version more awsome", RunningOnOperatingSystem="KOS0" });
+            list.Add(new ApplicationInfo() {ApplicationName ="kenneths app 3", ApplicationVersion = "Version most awsome", RunningOnOperatingSystem="KOS2" });
+
+            return list;
+        }
+
+        public SlaveConnection GetSlave(ApplicationInfo appInfo, PrimaryKey primaryKey)
+        {
+            //TODO FIX THIS M8
+            return new SlaveConnection()
+            {
+                IP = new IP() { TheIP = "0.0.0..0.000." },
+                OwnerPrimaryKey = new PrimaryKey() { TheKey = -1 },
+                Port = new Port() { ThePort=-1},
+                SlaveID = new SlaveID() {ID="-1" }
+            };
+        }
+
+    }
+}
